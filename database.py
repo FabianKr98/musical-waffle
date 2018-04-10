5b24749efe455cf618aa90b7d0c6c129c8984b39
FabianKr98 <fabifabi98@gmx.de>
2018-04-10 10:25:49 +0200

    datenbank1

---
+++ database.py
@@ -0,0 +1,6 @@
+data = []
+data.append(["Veit", "Stefan", "alring11312","098134891389"])
+data.append(["Eyberg", "Vinceent", "alring33903", "0908898"])
+data.append(["Faiß", "Janina", "llalal", "4378484"])
+print(data)
+for d in data: print(d)
